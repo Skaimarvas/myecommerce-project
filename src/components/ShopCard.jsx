f72c292735cc294fb42b1580301e465990049d3e
skaimarvas <skaimarvas@gmail.com>
2023-12-12 22:57:18 +0300

    shopcard oluşturuldu

---
+++ src/components/ShopCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import shopcard1 from "../assets/shopcard1.jpeg";
+
+export default function ShopCard() {
+  return (
+    <div className="relative">
+      <img
+        src={shopcard1}
+        alt="image 1"
+        className=" w-[206px] h-[223px] object-cover"
+      />
+      <div className="absolute inset-0 flex flex-col items-center justify-center  text-white gap-[9.5px]">
+        <h5 className="text-center text-base font-bold">CLOTHS</h5>
+        <span className="text-[14px] leading-5">5 Items</span>
+      </div>
+    </div>
+  );
+}
--- src/pages/productlist.jsx
+++ src/pages/productlist.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { MdNavigateNext } from "react-icons/md";
+import ShopCard from "../components/ShopCard";
 
 export default function ProductList() {
-  return <div>productlist</div>;
+  return (
+    <div className="tracking-wider">
+      <div className="flex justify-center py-[24px]">
+        <div className="flex flex-wrap justify-between items-center w-4/5 bg-[#FAFAFA]">
+          <div>
+            <h3 className="text-[#252B42] text-2xl font-bold">Shop</h3>
+          </div>
+          <div className="flex flex-row gap-[15px] items-center py-[15px] ">
+            <Link to="/">
+              {" "}
+              <span className="text-[#252B42] text-base font-bold">
+                Home
+              </span>{" "}
+            </Link>
+            <MdNavigateNext className="text-[25px] text-[#BDBDBD]" />
+            <span className="text-[#BDBDBD] text-base font-bold text-center">
+              Shop
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap gap-[15px] w-4/5">
+        <ShopCard />
+        <ShopCard />
+        <ShopCard />
+        <ShopCard />
+        <ShopCard />
+      </div>
+    </div>
+  );
 }
