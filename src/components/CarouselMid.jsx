c3ff78c8825b382fc902fda3f44c74ab9b7d696e
skaimarvas <skaimarvas@gmail.com>
2023-12-17 12:08:31 +0300

    fluid-cont ve sliderlar respomsive yapıldı.

--- src/components/CarouselMid.jsx
+++ src/components/CarouselMid.jsx
@@ -7,7 +7,9 @@ export default function CarouselMid() {
   return (
     <>
       <Carousel
-        className="tracking-wider"
+        autoplay
+        loop
+        className="tracking-wider bg-gradient-to-r from-[#00000059] to-[#00000059] sm:flex sm:items-center"
         navigation={({ setActiveIndex, activeIndex, length }) => (
           <div className="absolute bottom-4 left-2/4  flex -translate-x-2/4 ">
             {new Array(length).fill("").map((_, i) => (
@@ -24,19 +26,21 @@ export default function CarouselMid() {
           </div>
         )}
       >
-        <div className=" flex justify-center items-center   bg-[#23856D]   h-full w-full text-white pt-[106px] ">
-          <div className=" flex flex-wrap justify-center   items-center gap-[30px]  ">
-            <div className=" flex flex-col text-left gap-[35px] max-w-[509px]">
-              <h5 className="font-bold text-base ">SUMMER 2020</h5>
-              <h2 className="text-[58px] font-bold leading-[80px] ">
+        <div className="bg-[#23856D] h-full w-full text-white pt-[106px] tracking-wider ">
+          <div className=" flex flex-wrap justify-center   items-center gap-[30px] sm:px-[20px]  ">
+            <div className=" flex flex-col text-left gap-[35px] max-w-[509px] sm:items-center">
+              <h5 className="font-bold text-base sm:text-center sm:text-[20px] sm:font-normal sm:leading-[30px] ">
+                SUMMER 2020
+              </h5>
+              <h2 className="text-[58px] font-bold leading-[80px] sm:text-[40px] sm:leading-[50px] sm:text-center  ">
                 {" "}
                 Vita Classic Product{" "}
               </h2>
-              <h5 className="text-[20px] leading-[30px] ">
+              <h5 className="text-[20px] leading-[30px] sm:text-center ">
                 We know how large objects will act, We know how are objects will
                 act, We know
               </h5>
-              <div className="flex flex-wrap items-center gap-[34px]">
+              <div className="flex flex-wrap items-center gap-[34px] sm:flex-col sm:gap-[20px] ">
                 <span className="text-2xl font-bold text-center ">$16.48</span>
                 <button className="bg-[#2DC071] hover:bg-[#25a15f] shadow-lg rounded-md py-[15px] px-[40px] max-w-[221px]">
                   <span className="text-[14px] text-center leading-[22px] font-bold ">
@@ -54,14 +58,13 @@ export default function CarouselMid() {
             </div>
           </div>
         </div>
-
-        <div className="relative">
+        <div className="relative  ">
           <img
-            className="h-screen w-full object-cover"
+            className="h-screen  w-full object-cover sm:shadow-lg"
             src={photo3}
             alt="image 1"
           />
-          <div className="flex  items-end py-[112px]  justify-center absolute inset-0  h-full w-full text-white bg-gradient-to-r from-[#00000059] to-[#00000059] ">
+          <div className="flex  items-end py-[112px]  justify-center absolute inset-0  h-screen  w-full text-white bg-gradient-to-r from-[#00000059] to-[#00000059] ">
             <div className="flex flex-row justify-center gap-[30px] py-[48px] w-[1044px]">
               <div className="flex flex-col items-center  text-left gap-[24px] py-[40px] max-w-[580px]">
                 <h2 className="text-[58px] font-bold leading-[80px]  text-center">
--- src/components/ContainerFluid.jsx
+++ src/components/ContainerFluid.jsx
@@ -4,24 +4,26 @@ import fluid1 from "../assets/fluid1.png";
 
 export default function ContainerFluid() {
   return (
-    <div className="flex flex-wrap gap-[30px] justify-center items-center tracking-wider">
-      <div>
+    <div className="flex flex-wrap-reverse gap-[30px] justify-center items-center tracking-wider sm:px-[10px]">
+      <div className="sm:px-[10px]">
         <img
           src={fluid1}
           alt="image 1 "
-          className="w-[600px] h-[600px] object-cover object-center "
+          className="w-[600px] h-[600px] sm:max-w-[600px] sm:h-full object-cover sm:object-center "
         />
       </div>
-      <div className=" flex flex-col text-left gap-[35px] max-w-[509px]">
-        <h5 className="font-bold text-[#BDBDBD] text-base ">SUMMER 2020</h5>
-        <h2 className="text-[40px] text-[#252B42] font-bold leading-[50px] ">
+      <div className=" flex flex-col text-left gap-[35px] max-w-[509px] sm:items-center ]">
+        <h5 className="font-bold text-[#BDBDBD] text-base sm:text-center ">
+          SUMMER 2020
+        </h5>
+        <h2 className="text-[40px] text-[#252B42] font-bold leading-[50px] sm:text-center">
           {" "}
           Part of the Neural Universe{" "}
         </h2>
-        <h5 className="text-[20px] text-[#737373] leading-[30px] ">
+        <p className="text-[20px] text-[#737373] leading-[30px] sm:text-center ">
           We know how large objects will act, but things on a small scale.
-        </h5>
-        <div className="flex flex-wrap items-center gap-[10px]">
+        </p>
+        <div className="flex flex-wrap items-center justify-center gap-[10px]">
           <button className="bg-[#2DC071] hover:bg-[#25a15f] shadow-lg rounded-md py-[15px] px-[40px] max-w-[221px]">
             <span className="text-[14px] text-white text-center leading-[22px] font-bold ">
               ADD TO CART
