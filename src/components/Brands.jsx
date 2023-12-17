54d844da770bc77cf220c6308e3a7edbd58079c1
skaimarvas <skaimarvas@gmail.com>
2023-12-18 01:20:44 +0300

    brandsler düzeltildi.

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -5,18 +5,18 @@ import brand2 from "../assets/logos/fa-brands-2.png";
 import brand3 from "../assets/logos/fa-brands-3.png";
 import brand4 from "../assets/logos/fa-brands-4.png";
 import brand5 from "../assets/logos/fa-brands-5.png";
-import brand6 from "../assets/logos/fa-brands-6.svg";
+import brand6 from "../assets/logos/fa-brands-6.png";
 
 export default function Brands() {
   return (
-    <div className="flex flex-col items-center w-full bg-[#FAFAFA] py-[50px]">
-      <div className="flex flex-wrap justify-around items-center  w-4/5 ">
+    <div className="flex justify-center w-full bg-[#FAFAFA] ">
+      <div className="flex flex-wrap justify-around items-center py-[50px] w-4/5 sm:flex-col sm:gap-[60px] ">
         <img src={brand1} alt="" />
-        <img src={brand2} alt="" className="" />
-        <img src={brand3} alt="" className="" />
-        <img src={brand4} alt="" className="" />
-        <img src={brand5} alt="" className="" />
-        <img src={brand6} alt="" className="" />
+        <img src={brand2} alt="" />
+        <img src={brand3} alt="" />
+        <img src={brand4} alt="" />
+        <img src={brand5} alt="" />
+        <img src={brand6} alt="" />
       </div>
     </div>
   );
