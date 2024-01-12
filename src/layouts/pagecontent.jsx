127e8e27a3e3eb4572db67d272968daa5bbd78ca
skaimarvas <skaimarvas@gmail.com>
2024-01-12 23:00:18 +0300

    home uzantısı lowercase yazıldı.

--- src/layouts/pagecontent.jsx
+++ src/layouts/pagecontent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 //Pages
-import Home from "../pages/Home";
+import Home from "../pages/home";
 import ProductList from "../pages/ProductList";
 import Product from "../pages/Product";
 import About from "../pages/About";
