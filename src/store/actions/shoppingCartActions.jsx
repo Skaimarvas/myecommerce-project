c5c48ff262dda5353ddbf1a1a73408db1783dc5d
skaimarvas <skaimarvas@gmail.com>
2024-02-02 12:26:36 +0300

    CLEAR CART actionu ShoppingCartActions'a eklendi.

--- src/store/actions/shoppingCartActions.jsx
+++ src/store/actions/shoppingCartActions.jsx
@@ -9,7 +9,13 @@ export const GET_ADDRESS = "GET_ADDRESS";
 export const POST_ADDRESS = "POST_ADDRESS";
 export const GET_PAYMENT = "GET_PAYMENT";
 export const POST_PAYMENT = "POST_PAYMENT";
+export const CLEAR_CART = "CLEAR_CART";
 
+export const clearCart = () => {
+  return {
+    type: CLEAR_CART,
+  };
+};
 export const getAddressData = (adress) => {
   return {
     type: GET_ADDRESS,
