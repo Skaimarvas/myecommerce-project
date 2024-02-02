39985405845dae7c91ce0e0202017ec9a0a23262
skaimarvas <skaimarvas@gmail.com>
2024-02-02 12:17:01 +0300

    Address formunda işlem tamamlandığında pencere kapatma işlevi getirildi.

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -14,6 +14,7 @@ export default function Address(props) {
   const dataHandle = (data) => {
     console.log("DATA", data);
     dispatch(postAddress(data));
+    setClose(false);
   };
 
   return (
